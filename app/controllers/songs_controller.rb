38802f8df6d069a1b840b33b273f72d614053b35
Rachel Wyatt <rw126@evansville.edu>
2019-02-17 21:27:09 +0000

    Automatically backed up by Learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,10 +1,17 @@
 class SongsController < ApplicationController
+  def new 
+    @song = Song.new
+  end
+
   def create
+    @song = Song.new(song_params(:title, :description))
+    @song.save
+    redirect_to song_path(@song)
   end
 
   def update
     @song = Song.find(params[:id])
-    @song.update(song_params[:name], [:artist_id], [:genre_id])
+    @song.update(song_params(:name, :artist_id, :genre_id))
     redirect_to song_path(@song)
   end
 
