4a7b8228dcc4dc6f564fe9f3a300763f3da13a10
Rachel Wyatt <rw126@evansville.edu>
2019-02-17 21:23:08 +0000

    Automatically backed up by Learn

--- app/controllers/genres_controller.rb
+++ app/controllers/genres_controller.rb
@@ -11,7 +11,7 @@ class GenresController < ApplicationController
 
   def update
     @genre = Genre.find(params[:id])
-    @genre.update(genre_params(:tname))
+    @genre.update(genre_params(:name))
     redirect_to genre_path(@genre)
   end
 
